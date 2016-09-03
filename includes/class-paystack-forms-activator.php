e551e6b931e0fca150029d7f59fdf20d792e10f2
Douglas Kesi-Ayeba Kendyson <kendyson@kendyson.com>
2016-09-04 00:58:47 +0100

    create table on activation

--- includes/class-paystack-forms-activator.php
+++ includes/class-paystack-forms-activator.php
@@ -30,7 +30,29 @@ class Paystack_Forms_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'paystack_forms_payments';
 
+		$sql = "CREATE TABLE $table_name (
+			id int(11) NOT NULL AUTO_INCREMENT,
+			post_id int(11) NOT NULL,
+		  user_id int(11) NOT NULL,
+		  email varchar(255) DEFAULT '' NOT NULL,
+		  metadata text,
+		  paid int(1) NOT NULL DEFAULT '0',
+			txn_code varchar(255) DEFAULT '' NOT NULL,
+		  amount varchar(255) DEFAULT '' NOT NULL,
+		  views smallint(5) NOT NULL,
+			created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		  modified datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		  UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
 	}
+
+
 
 }
