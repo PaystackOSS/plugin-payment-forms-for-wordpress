d7d08a4bf913b7c655f112d953d1d107575a7911
ibrahimlawal <ibrahim@lawal.me>
2017-10-10 23:24:02 +0100

    Use PaystackFee object when calculating fees

--- public/js/paystack-forms-public.js
+++ public/js/paystack-forms-public.js
@@ -1,3 +1,82 @@
+function KkdPffPaystackFee() {
+    this.DEFAULT_PERCENTAGE = 0.015;
+    this.DEFAULT_ADDITIONAL_CHARGE = 10000;
+    this.DEFAULT_THRESHOLD = 250000;
+    this.DEFAULT_CAP = 200000;
+
+    this.percentage = this.DEFAULT_PERCENTAGE;
+    this.additional_charge = this.DEFAULT_ADDITIONAL_CHARGE;
+    this.threshold = this.DEFAULT_THRESHOLD;
+    this.cap = this.DEFAULT_CAP;
+
+    this.chargeDivider = 0;
+    this.crossover = 0;
+    this.flatlinePlusCharge = 0;
+    this.flatline = 0;
+
+    this.withPercentage = function (percentage) {
+        this.percentage = percentage;
+        this.__setup();
+    };
+
+    this.withAdditionalCharge = function (additional_charge) {
+        this.additional_charge = additional_charge;
+        this.__setup();
+    };
+
+    this.withThreshold = function (threshold) {
+        this.threshold = threshold;
+        this.__setup();
+    };
+
+    this.withCap = function (cap) {
+        this.cap = cap;
+        this.__setup();
+    };
+
+    this.__setup = function () {
+        this.chargeDivider = this.__chargeDivider();
+        this.crossover = this.__crossover();
+        this.flatlinePlusCharge = this.__flatlinePlusCharge();
+        this.flatline = this.__flatline();
+    };
+
+    this.__chargeDivider = function () {
+        return 1 - this.percentage;
+    };
+
+    this.__crossover = function () {
+        return (this.threshold * this.chargeDivider) - this.additional_charge;
+    };
+
+    this.__flatlinePlusCharge = function () {
+        return (this.cap - this.additional_charge) / this.percentage;
+    };
+
+    this.__flatline = function () {
+        return this.flatlinePlusCharge - this.cap;
+    };
+
+    this.addFor = function (amountinkobo) {
+        if (amountinkobo > this.flatline)
+            return parseInt(Math.round(amountinkobo + this.cap));
+        else if (amountinkobo > this.crossover)
+            return parseInt(Math.round((amountinkobo + this.additional_charge) / this.chargeDivider));
+        else
+            return parseInt(Math.round(amountinkobo / this.chargeDivider));
+    };
+
+
+    this.__setup = function () {
+        this.chargeDivider = this.__chargeDivider();
+        this.crossover = this.__crossover();
+        this.flatlinePlusCharge = this.__flatlinePlusCharge();
+        this.flatline = this.__flatline();
+    };
+
+    this.__setup();
+}
+
 (function( $ ) {
 	'use strict';
 
@@ -83,20 +162,14 @@
 					 	var name = $('#pf-vamount option:selected').attr('data-name');
 			 			$('#pf-vname').val(name);
 					}
-     				 var multiplier = 0.015;
-					 var fees = multiplier * transaction_amount;
-					 var extrafee = 0;
-					 if (fees > 2000) {
-						 var fees = 2000;
-					 }else{
-						 if (transaction_amount > 2500) {fees += 100};
-					 }
-					 var total = transaction_amount + fees;
-								//  console.log(transaction_amount);
-		             if (transaction_amount == '' || transaction_amount == 0 || transaction_amount.length == 0 || transaction_amount == null || isNaN (transaction_amount)) {
-						 var total = 0;
-						 var fees = 0;
-					 }
+                    if (transaction_amount == '' || transaction_amount == 0 || transaction_amount.length == 0 || transaction_amount == null || isNaN (transaction_amount)) {
+						var total = 0;
+						var fees = 0;
+					} else {
+                        var obj = new KkdPffPaystackFee();
+                        var total = obj.addFor(transaction_amount * 100) / 100;
+                        var fees = total - transaction_amount;
+					}
 					 $(".pf-txncharge").hide().html("NGN"+fees.toFixed(2)).show().digits();
 					 $(".pf-txntotal").hide().html("NGN"+total.toFixed(2)).show().digits();
 		         }, 100);
