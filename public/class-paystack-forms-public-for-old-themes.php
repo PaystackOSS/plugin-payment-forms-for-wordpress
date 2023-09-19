88a2490768c277b3e4573f9a2aea10d538ce5516
Segun <oniludesegun@gmail.com>
2023-09-19 19:52:39 +0100

    fixed invalid amount issue for transactions

--- public/class-paystack-forms-public-for-old-themes.php
+++ public/class-paystack-forms-public-for-old-themes.php
@@ -1642,14 +1642,14 @@ function kkd_pff_paystack_confirm_payment()
                         $result = "success";
                     // kkd_pff_paystack_send_receipt($currency,$amount,$name,$payment_array->email,$code,$metadata)
                     } else {
-                        $usequantity = get_post_meta($payment_array->post_id, '_usequantity', true);
-                        if ($usequantity == 'no') {
-                            $oamount = (int)str_replace(' ', '', $amount);
-                        } else {
-                            $quantity = $_POST["quantity"];
-                            $unitamount = (int)str_replace(' ', '', $amount);
-                            $oamount = $quantity*$unitamount;
-                        }
+                        // $usequantity = get_post_meta($payment_array->post_id, '_usequantity', true);
+                        // if ($usequantity == 'no') {
+                        //     $oamount = (int)str_replace(' ', '', $amount);
+                        // } else {
+                        //     $quantity = $_POST["quantity"];
+                        //     $unitamount = (int)str_replace(' ', '', $amount);
+                        //     $oamount = $quantity*$unitamount;
+                        // }
                         if ($txncharge == 'customer') {
                             if ($minimum == 0 && $amount != 0) {
                                 $oamount = kkd_pff_paystack_add_paystack_charge($oamount);
